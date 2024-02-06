81d390e23cc448926a89f9e5c0b2322c0a054bab
isaackouka225 <isaacsavadogo225@gmail.com>
2024-02-06 17:51:21 +0100

    obstacle  commit

--- app.js
+++ app.js
@@ -13,11 +13,20 @@ document.addEventListener('DOMContentLoader', () => {
     
     // Fonctions du jeu
     function startGame() {
+      
+      birdBottom -= gravity;
+      bird.style.bottom = birdBottom + 'px';
+      bird.style.left = birdLeft + 'px';
+      if (isGameOver) return;
       // définir la position de l'oiseau au début de la partie
     }
     let gameTimerId = setInterval(startGame, 20)
   
     function control(e) {
+      if (e.type === "click"  || (e.type === "space" && e.code === "Space")) {
+        jump ();
+      }
+           
       // utiliser e (event) pour cibler la barre espace
       // Mots clés qu'on avait trouvé pour faire une recherche 
       // sur comment cibler un espace : "key, space, eventListeners"
@@ -26,23 +35,55 @@ document.addEventListener('DOMContentLoader', () => {
     }
   
     function jump() {
+      if (birdBottom < 100){
+        birdBottom += 10
+        bird.stylebottom = 'px'
+      }
       // attention à bien faire en sorte que l'oiseau 
       // ne puisse sauter que quand il n'a pas dépassé le border-top
   
       // changer le style bottom du bird, pour changer sa position
     }
-    document.addEventListener(/* barre espace */_, control)
+    document.addEventListener(/* barre espace */ "space", control)
+    document.getElementById("play").addEventListener("click", control)
   
     function generateObstacle() {
       let obstacleLeft = 500
-      let randomHeight = 0 /* mettre une hauteur random */
+      let randomHeight = Math.random() * 150; /* mettre une hauteur random */
       let obstacleBottom = randomHeight
   
       // générer les obstacles avec document.createElement('div')
+
+      const obstacle = document.createElement('div');
+      const topObstacle = document.createElement('div');
+      obstacle.classList.add('obstacle');
+      topObstacle.classList.add('topObstacle');
+      gameDisplay.appendChild(obstacle);
+      gameDisplay.appendChild(topObstacle);
+      obstacle.style.left = obstacleLeft + 'px';
+      topObstacle.style.left = obstacleLeft + 'px';
+      obstacle.style.bottom = obstacleBottom + 'px';
+      topObstacle.style.bottom = obstacleBottom + gap + 'px';
   
       // si la partie continue, ajouter des obstacles
   
+      obstacle.style.backgroundImage = "url('assets/flappybird-pipe.png')";
+      topObstacle.style.backgroundImage = "url('assets/flappybird-pipe.png')";
+      
       // créer une fonction qui bouge l'obstacle vers la gauche à chaque frame
+
+      function moveObstacle() {
+        obstacleLeft -= 2; 
+        obstacle.style.left = obstacleLeft + 'px';
+        topObstacle.style.left = obstacleLeft + 'px';
+
+       // Si l'obstacle est hors de la vue, le supprimer et générer un nouveau
+      if (obstacleLeft < -60) {
+        gameDisplay.removeChild(obstacle);
+        gameDisplay.removeChild(topObstacle);
+        score++; // augmenter le score ici si nécessaire
+        generateObstacle();
+      }
     }
   
     function gameOver() {
